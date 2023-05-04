8beb468e5932a15e84c48f42a1fc44825a577e80
SantyPrado01 <santiago01prado@gmail.com>
2023-05-03 21:32:49 -0300

    Creacion de Opcion 3

--- Funciones_Software/Funciones.py
+++ Funciones_Software/Funciones.py
@@ -1,5 +1,14 @@
 import random
 from fractions import Fraction
+from math import sqrt
+
+def esNumero(a):
+    try:
+        float(a)
+        return True
+    except ValueError:
+        return False
+
 
 def rectaParalela(a,b):
     cont = 0
@@ -32,6 +41,10 @@ def rectaPerpendicular (a,b):
 
 
 def recta(a,b):
+    # y = mx + b
+    # 0 = mx + b
+    # x = -b/m
+    
     if a > 0:
         pendiente = 'Creciente'
     else:
@@ -41,4 +54,28 @@ def recta(a,b):
     print('Corte en X = ' + str(raiz))
     print('Corte en Y = ' + str(a))
     print('Pendiente = ' + str(pendiente))
+
+
+def parabola(a,b,c):
     
+    if (b*b - 4*a*c) < 0:
+        print("La parábola no tiene soluciones reales.")
+    elif (b*b - 4*a*c)== 0:
+        x = -b / (2*a)
+        print("La parábola toca el eje x en un solo punto: " + str(x)) #Como el discriminante es 0 se reduce la expresion para calcular la raiz
+    else:
+        x1 = (-b+sqrt(b*b-4*a*c))/(2*a)  # Fórmula de Bhaskara parte positiva
+        x2 = (-b-sqrt(b*b-4*a*c))/(2*a)  # Fórmula de Bhaskara parte negativa
+        print('Las soluciones de la ecuación son: \n x1= ' + "{0:.2f}".format(x1), ' \n x2= ' + "{0:.2f}".format(x2))
+        
+    if a>0:
+        print ("La parabola es concava hacia arriba")
+        print(f"El intervalo de decrecimiento del infinito hacia {-b/(2*a)},y crecimiento desde {-b/(2*a)} al Infinito ")
+    elif a<0:
+            print ("La parabola en concava hacia abajo")
+            print(f"El intervalo de crecimiento desde el infinito hasta {(-b/2*a)}, y decrecimiento desde {(-b/2*a)} al Infinito")      
+    else:
+        print("Si a es igual a 0 la funcion no es cuadratica")            
+        print("El corte con el eje y es: ",c )
+    
+parabola(3,8,3)
--- Funciones_Software/Matematica.py
+++ Funciones_Software/Matematica.py
@@ -19,52 +19,101 @@
 # intervalo de crecimiento e intervalo de decrecimiento, coordenadas del vértice, concavidad de la parábola 
 # (cóncava hacia arriba o cóncava hacia abajo). Además deberá informarse en el caso que la parábola no posea soluciones 
 # reales como así también cuando la solución tenga doble multiplicidad.
-import os
+
 import random 
 from Funciones import *
 
-bandera = 0 
-contador = 0
 
+while True:
+    opcion = input('Bienvenido al Programa \n Ingrese 1 - Para rectas paralelas y perpendiculares a una dada \n Ingrese 2 - Para analisis de una Funcion Lineal \n Ingrese 3 - Para Analisis de una funcion cuadratica \n Que opcion deseas realizar? :')
+    
+    if esNumero(opcion):
+        
+        opcion = int(opcion)
 
-while bandera == 0:
-    opcion = int(input('Bienvenido al Programa \n Ingrese 1 - Para rectas paralelas y perpendiculares a una dada \n Ingrese 2 - Para analisis de una Funcion Lineal \n Ingrese 3 - Para Analisis de una funcion cuadratica \n Que opcion deseas realizar? :'))
-    if opcion == 1:
-        bandera = 1
-        print('Estas en la opcion uno, elige dos numeros uno sera el coeficiente principal y otro el termino independiente, luego generare 3 rectas paralelas y 3 perpendiculares.')
+#Comienzo de la Opcion 1
+
+        if opcion == 1:
+
+            print('Estas en la opcion uno, elige dos numeros uno sera el coeficiente principal y otro el termino independiente, luego generare 3 rectas paralelas y 3 perpendiculares.')
         
-        cPrincipal = int(input('Ingresa el coeficiente principal: '))
-        while cPrincipal == 0:
-            print('El coeficiente principal tiene que ser mayor a cero, ingresa un numero mayor a cero')
-            cPrincipal = int(input('Ingresa el coeficiente principal: '))
-        tIndependiente = int(input('Ingresa el termino independiente: '))
+            print('-_' * 45)
         
-        tIndependiente = int(input('Ingresa el termino independiente: '))
-        
-        print('--------------------------------------------------------')
-        print("La condicion de paralelismo es que el coheficiente principal se mantenga y el termino independiente sea el que cambie. \nEjemplos de ecuaciones con rectas paralelas a la dada son:")
-        rectaParalela(cPrincipal,tIndependiente)
-        print('--------------------------------------------------------')
-        print("La condicion de perpendicularidad es que la pendiente debe ser inversa y opuesta, el termino independiente puede cambiar o no hacerlo. \nEjemplos de ecuacion con rectas perpendiculares a la dada son:")
-        rectaPerpendicular(cPrincipal, tIndependiente)
-        print('------------------------------------------')
-        
+            cPrincipal = input('Ingresa el coeficiente principal: ')
+            tIndependiente = input('Ingresa el termino independiente: ')
+
+            if esNumero(cPrincipal) and esNumero (tIndependiente):
+                
+                cPrincipal = float(cPrincipal)
+                tIndependiente = float(tIndependiente)
+
+                while cPrincipal == 0:
+                    
+                    print('El coeficiente principal tiene que ser mayor a cero, ingresa un numero mayor a cero')
+                    cPrincipal = float(input('Ingresa el coeficiente principal: '))
+                    
+                print('-_' * 45)
+                print("La condicion de paralelismo es que el coheficiente principal se mantenga y el termino independiente sea el que cambie. \nEjemplos de ecuaciones con rectas paralelas a la dada son:")
+                rectaParalela(cPrincipal,tIndependiente)
+                print('-_' * 45)
+                print("La condicion de perpendicularidad es que la pendiente debe ser inversa y opuesta, el termino independiente puede cambiar o no hacerlo. \nEjemplos de ecuacion con rectas perpendiculares a la dada son:")
+                rectaPerpendicular(cPrincipal, tIndependiente)
+                print('-_' * 45 )
+                break
+                
+            else:
+                
+                print('Tienes que ingresar un numero, intena nuevamente')
+    
+#Final de la Opcion 1
             
-                             
-    elif opcion == 2:
-        bandera = 1
-        print('Estas en la opcion dos')
-        cPrincipal = int(input('Ingresa el coeficiente principal: '))
-        if cPrincipal == 0:
-            print('El coeficiente principal tiene que ser mayor a cero, ingresa un numero mayor a cero')
-            cPrincipal = int(input('Ingresa el coeficiente principal: '))
+#Comienzo de la Opcion 2
+                          
+        elif opcion == 2:
+
+            print('Estas en la opcion dos, elige dos numeros uno sera el coeficiente principal y el otro el termino independiente. Con estos datos te mostrare la siguiente información: corte con el eje x, corte con el eje y, comportamiento de la recta')
+     
+            print('-_' * 45)
+            
+            cPrincipal = input('Ingresa el coeficiente principal: ')
+            tIndependiente = input('Ingresa el termino independiente: ')
+            
+            if esNumero(cPrincipal) and esNumero(tIndependiente):
+                
+                cPrincipal = float(cPrincipal)
+                tIndependiente = float(tIndependiente)
+                
+                while cPrincipal == 0:
+                    print('El coeficiente principal tiene que ser mayor a cero, ingresa un numero mayor a cero')
+                    cPrincipal = float(input('Ingresa el coeficiente principal: '))
         
-        tIndependiente = int(input('Ingresa el termino independiente: '))
+                recta(cPrincipal,tIndependiente)
+                break
+            else:
+                
+                print('Tienes que ingresar un numero, intenta nuevamente')
+                
+#Fin de la Opcion 2
         
-        recta(cPrincipal,tIndependiente)
-    elif opcion == 3:
-        bandera = 1
-        print('Estas en la opcion 1')
+#Comienzo de la Opcion 3
+
+        elif opcion == 3:
+ 
+            print('Estas en la opcion 3')
+        
+            cPrincipal = input('Ingresa el coeficiente principal: ')
+            cLineal = input('Ingresa el coeficiente lineal: ')
+            tIndependiente = input('Ingresa el termino independiente: ')
+        
+            if esNumero(cPrincipal) and esNumero(cLineal) and esNumero(tIndependiente):
+                cPrincipal = float(cPrincipal)
+                cLineal = float(cLineal)
+                tIndependiente = float(tIndependiente)    
+                parabola(cPrincipal, cLineal,tIndependiente)
+                    
+            else:
+                print('Tienes que ingresar un numero, intenta nuevamente')    
+              
     else:
-        bandera = 0
+        print('Opcion No programada, intente nuevamente')
         
